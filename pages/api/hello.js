73f3c503ad84446199a53491121bfc5d425e7d23
mdasadulgoni <mdasadulgoni@gmail.com>
2023-10-12 22:25:51 +0530

    c3

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,7 +4,19 @@
 
 // 2. Define Area
  function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // function serializations
+  // object.method().method(actualArgument)
+     res.status(200).json(
+      {
+        ncatid:`${req.query.ncatid}`,
+        area:`${req.query.area}`,
+        search:`${req.query.search}`,
+        mncatname:`${req.query.mncatname}`,
+        search_id:`${req.query.search_id}`,
+        abd_btn:`${req.query.abd_btn}`,
+        abd_heading:`${req.query.abd_heading}`,
+      }
+     )
 }
 
 // 3. Export Area
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React from 'react'
+import { Box, Container, CssBaseline } from '@mui/material'
 
 export default function Home() {
   return (
-    <>
-    <h2>1. I Created Project NextJs 12.0.0 Version </h2>
-    <h2>2. I Created New Repo...</h2>
-    <h2>3. I Deployed On Vercel</h2>
-    </>
+    <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <h2>1. I Created Project NextJs 12.0.0 Version </h2>
+        <h2>2. I Created New Repo.. in GitHub</h2>
+        <h2>3. I Created Some Rest API </h2>
+        <h2>3. I Created Basic MUI Container </h2>
+        <h2>4. I Deployed On Vercel</h2>
+      </Box>
+    </Container>
+    </React.Fragment>
   )
 }
